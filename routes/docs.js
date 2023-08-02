1a942eb3de8d5513efcef6908a49ae0906f77bd9
marcin bułka <mbulka44@gmail.com>
2023-08-02 22:00:14 +0200

    adding notes and page with columns documentation

--- routes/docs.js
+++ routes/docs.js
@@ -4,12 +4,28 @@ const router = express.Router()
 const docs = [{
     id: 0,
     table_name: 'table1',
-    description: 'description1'
+    description: 'description1',
+    columns: [{
+        column_name: 'col11',
+        column_description: 'this is column 1 from table 1'
+    },
+    {
+        column_name: 'col12',
+        column_description: 'this is column 2 from table 1'
+    }]
 },
 {
     id: 1,
     table_name: 'table2',
-    description: 'description2'
+    description: 'description2',
+    columns: [{
+        column_name: 'col21',
+        column_description: 'this is column 1 from table 2'
+    },
+    {
+        column_name: 'col22',
+        column_description: 'this is column 2 from table 2'
+    }]
 }]
 
 router.get('/', (req, res) => {
