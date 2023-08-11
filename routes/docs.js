d2004ac14d2f3cbdb355f3b07a6b888b52fe0cf9
Marcin Bułka <mbulka@sdl.com>
2023-08-11 13:19:19 +0200

    small change

--- routes/docs.js
+++ routes/docs.js
@@ -2,72 +2,10 @@ const express = require('express')
 const Doc = require('./../models/docs')
 const router = express.Router()
 
-// class Model{
-//     async load_model(){
-//         // loading a model in the ONNX format prepared by the script ml_model/model_preparation.py
-//         const { AutoModel, AutoTokenizer, env } = await import('@xenova/transformers')
-//         // path indicating where is the model which we want to load
-//         env.localModelPath = __dirname + '/../ml_model/models/model'
-//         this.tokenizer = await AutoTokenizer.from_pretrained('')
-//         this.model = await AutoModel.from_pretrained('')
-        
-//         return [this.model, this.tokenizer]
-//     }
-// }
-class Model{
-    async load_model(){
-        // loading a model in the ONNX format prepared by the script ml_model/model_preparation.py
-        const { AutoModel, AutoTokenizer, env } = await import('@xenova/transformers')
-        // path indicating where is the model which we want to load
-        env.localModelPath = __dirname + '/../ml_model/models/model'
-        this.tokenizer = await AutoTokenizer.from_pretrained('')
-        this.model = await AutoModel.from_pretrained('')
-        
-        return [this.model, this.tokenizer]
-    }
-
-    async predict(inputs){
-        // encode input text
-        inputs = await this.tokenizer(inputs)
-        let result = await this.model(inputs)
-        result = Array.from(result.last_hidden_state.data)
-        //reshaping result into a matrix of shape (sequence_length, 768)
-        result.reshape(result.length / 768, 768)
-        // mean pooling
-        // let sum = result[0]
-        // for (let row of result.slice(1)){
-        //     sum += row
-        // }
-        console.log([1,2] + [2,2])
-        return result
-    }
-}
-
-// function for reshaping 1d array into 2d array
-Array.prototype.reshape = function(rows, cols) {
-    var copy = this.slice(0); // Copy all elements.
-    this.length = 0; // Clear out existing array.
-  
-    for (var r = 0; r < rows; r++) {
-        var row = [];
-        for (var c = 0; c < cols; c++) {
-            var i = r * cols + c;
-            if (i < copy.length) {
-            row.push(copy[i]);
-            }
-        }
-        this.push(row);
-    }
-};
-
 router.post('/search', async (req, res) => {
     const searchedQuery = req.body.searchBar
-    
-    // const [model, tokenizer] = await new Model().load_model()
-    // let input = await tokenizer('translate English to German I love transformers');
-    // result = await model(input)
-
     const model = new Model()
+    
     await model.load_model()
     result = model.predict('I like dogs')
 
@@ -123,6 +61,59 @@ router.put('/:id/columns', async (req, res) => {
     await doc.save()
     res.redirect(`/docs/${req.params.id}/columns`)
 })
+
+
+class Model{
+    async load_model(){
+        // loading a model in the ONNX format prepared by the script ml_model/model_preparation.py
+        const { AutoModel, AutoTokenizer, env } = await import('@xenova/transformers')
+        // path indicating where is the model which we want to load
+        env.localModelPath = __dirname + '/../ml_model/models/model'
+        this.tokenizer = await AutoTokenizer.from_pretrained('')
+        this.model = await AutoModel.from_pretrained('')
+        
+        return [this.model, this.tokenizer]
+    }
+
+    async predict(inputs){
+        // encode input text
+        inputs = await this.tokenizer(inputs)
+        let result = await this.model(inputs)
+        result = Array.from(result.last_hidden_state.data)
+
+        //reshaping result into a matrix of shape (sequence_length, out_dim)
+        // out_dim is a dimension of a vector returned by a model for each input token
+        result.reshape(result.length / 768, 768)
+
+        // mean pooling
+        let sum = result[0]
+        for (let row of result.slice(1)){
+            sum = sum.map((num, idx) => {
+                return num + row[idx]
+            })
+        }
+        result = sum.map(value => value / result.length)
+
+        return result
+    }
+}
+
+// function for reshaping 1d array into 2d array
+Array.prototype.reshape = function(rows, cols) {
+    var copy = this.slice(0); // Copy all elements.
+    this.length = 0; // Clear out existing array.
+  
+    for (var r = 0; r < rows; r++) {
+        var row = [];
+        for (var c = 0; c < cols; c++) {
+            var i = r * cols + c;
+            if (i < copy.length) {
+            row.push(copy[i]);
+            }
+        }
+        this.push(row);
+    }
+};
 
 
 module.exports = router
